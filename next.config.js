67e3a2c8b4753586ed22bbb4c24716a1aeb1c57a
hrdtbs <harada.tsubasa.0422@gmail.com>
2020-03-01 02:22:42 +0900

    feat: use workbox

--- next.config.js
+++ next.config.js
@@ -1,4 +1,56 @@
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS({
-    target: "serverless"
-})
+const withCSS = require("@zeit/next-css")
+const withOffline = require("next-offline")
+const compose = require("next-compose")
+
+module.exports = compose([
+    [
+        withCSS,
+        {
+            target: "serverless"
+        }
+    ],
+    [
+        withOffline,
+        {
+            target: "serverless",
+            transformManifest: manifest => ["/"].concat(manifest), // add the homepage to the cache
+            // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+            // turn on the SW in dev mode so that we can actually test it
+            generateInDevMode: true,
+            workboxOpts: {
+                swDest: process.env.NEXT_EXPORT ? "service-worker.js" : "static/service-worker.js",
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https?.*/,
+                        handler: "NetworkFirst",
+                        options: {
+                            cacheName: "https-calls",
+                            networkTimeoutSeconds: 15,
+                            expiration: {
+                                maxEntries: 150,
+                                maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200]
+                            }
+                        }
+                    },
+                    {
+                        urlPattern: /.(?:png|webp|svg|mp4)$/,
+                        handler: "CacheFirst"
+                    }
+                ]
+            },
+            experimental: {
+                async rewrites() {
+                    return [
+                        {
+                            source: "/service-worker.js",
+                            destination: "/_next/static/service-worker.js"
+                        }
+                    ]
+                }
+            }
+        }
+    ]
+])
