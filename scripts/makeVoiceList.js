958f0527e1cefb0f350c6a075fc4d75bd031f5e1
hrdtbs <harada.tsubasa.0422@gmail.com>
2020-02-29 19:04:59 +0900

    fix: remove invalid audio

--- scripts/makeVoiceList.js
+++ scripts/makeVoiceList.js
@@ -6,7 +6,7 @@ const VOICE_DIR = path.resolve(__dirname, "../static/voices/")
 fs.readdir(VOICE_DIR, (err, files) => {
     if (err) throw err
     const fileList = files.filter(file => {
-        return fs.statSync(path.join(VOICE_DIR, file)).isFile() && /.*\.mp3$/.test(file)
+        return fs.statSync(path.join(VOICE_DIR, file)).isFile() && /.*\.mp4$/.test(file)
     })
     const f = fileList.map(file => path.basename(file, path.extname(file)))
     fs.writeFileSync(
