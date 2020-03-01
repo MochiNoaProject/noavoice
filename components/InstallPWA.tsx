2fce68e956ac8c605aa10ab62c658661af8042bd
hrdtbs <harada.tsubasa.0422@gmail.com>
2020-03-01 17:20:49 +0900

    hoge

--- components/InstallPWA.tsx
+++ components/InstallPWA.tsx
@@ -4,12 +4,38 @@ let _installPrompt: any
 if (typeof window !== "undefined") {
     window.addEventListener("beforeinstallprompt", e => {
         e.preventDefault()
+        console.error("beforeinstallprompt")
         _installPrompt = e
     })
 }
 
 export default function AppInstallButton() {
     const [prompt, setInstallPrompt] = useState<any>(_installPrompt)
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            window.addEventListener("beforeinstallprompt", async e => {
+                // beforeinstallprompt Event fired
+                try {
+                    // e.userChoice will return a Promise.
+                    //@ts-ignore
+                    const choiceResult = await e.userChoice
+                    if (choiceResult.outcome === "dismissed") {
+                        /* eslint-disable no-console */
+                        console.log("User cancelled home screen install")
+                        /* eslint-enable no-console */
+                    } else {
+                        /* eslint-disable no-console */
+                        console.log("User added to home screen")
+                        /* eslint-enable no-console */
+                    }
+                } catch (error) {
+                    /* eslint-disable no-console */
+                    console.error("user choice prompt promise failed to resolve, error: ", error)
+                    /* eslint-enable no-console */
+                }
+            })
+        }
+    }, [])
     useEffect(() => {
         if (_installPrompt) return
         const id = setInterval(() => {
