badf6533126794ce0354a77a5a58bfa6b93efa0b
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-03-02 01:33:03 +0900

    fix: upgrade to zero config for zeit now

--- next.config.js
+++ next.config.js
@@ -1,49 +1,61 @@
 const withOffline = require("next-offline")
-const compose = require("next-compose")
 
-module.exports = compose([
-    [
-        withOffline,
-        {
-            target: "serverless",
-            transformManifest: manifest => ["/"].concat(manifest), // add the homepage to the cache
-            // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
-            // turn on the SW in dev mode so that we can actually test it
-            generateInDevMode: true,
-            workboxOpts: {
-                swDest: process.env.NEXT_EXPORT ? "service-worker.js" : "static/service-worker.js",
-                runtimeCaching: [
-                    {
-                        urlPattern: /^https?.*/,
-                        handler: "NetworkFirst",
-                        options: {
-                            cacheName: "https-calls",
-                            networkTimeoutSeconds: 15,
-                            expiration: {
-                                maxEntries: 150,
-                                maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
-                            },
-                            cacheableResponse: {
-                                statuses: [0, 200]
-                            }
-                        }
+const nextConfig = {
+    transformManifest: manifest => ["/"].concat(manifest), // add the homepage to the cache
+    // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+    // turn on the SW in dev mode so that we can actually test it
+    generateInDevMode: true,
+    workboxOpts: {
+        swDest: process.env.NEXT_EXPORT ? "service-worker.js" : "static/service-worker.js",
+        runtimeCaching: [
+            {
+                urlPattern: /^https?.*/,
+                handler: "NetworkFirst",
+                options: {
+                    cacheName: "https-calls",
+                    networkTimeoutSeconds: 15,
+                    expiration: {
+                        maxEntries: 150,
+                        maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
                     },
-                    {
-                        urlPattern: /.(?:png|webp|svg|mp4)$/,
-                        handler: "CacheFirst"
+                    cacheableResponse: {
+                        statuses: [0, 200]
                     }
-                ]
+                }
             },
-            experimental: {
-                async rewrites() {
-                    return [
+            {
+                urlPattern: /.(?:png|webp|svg|mp4)$/,
+                handler: "CacheFirst"
+            }
+        ]
+    },
+    experimental: {
+        rewrites() {
+            return [
+                {
+                    source: "/service-worker.js",
+                    destination: "/_next/static/service-worker.js"
+                }
+            ]
+        },
+        headers() {
+            return [
+                {
+                    source: "/service-worker.js",
+                    headers: [
                         {
-                            source: "/service-worker.js",
-                            destination: "/_next/static/service-worker.js"
+                            key: "Cache-Control",
+                            value: "public, max-age=43200, immutable"
+                        },
+                        {
+                            key: "Service-Worker-Allowed",
+                            value: "/"
                         }
                     ]
                 }
-            }
+            ]
         }
-    ]
-])
+    }
+}
+
+module.exports = withOffline(nextConfig)
