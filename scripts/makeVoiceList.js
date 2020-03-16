680d973925decc34cbc7bdded5c2b326ef1da487
hrdtbs <harada.tsubasa.0422@gmail.com>
2020-03-17 01:31:01 +0900

    chore(scripts): change the extension to be output to the list from mp4 to mp3

--- scripts/makeVoiceList.js
+++ scripts/makeVoiceList.js
@@ -6,7 +6,7 @@ const path = require("path")
 fs.readdir(VOICE_DIR, (err, files) => {
     if (err) throw err
     const fileList = files.filter(file => {
-        return fs.statSync(path.join(VOICE_DIR, file)).isFile() && /.*\.mp4$/.test(file)
+        return fs.statSync(path.join(VOICE_DIR, file)).isFile() && /.*\.mp3$/.test(file)
     })
     const f = fileList.map(file => path.basename(file, path.extname(file)))
     fs.writeFileSync(
